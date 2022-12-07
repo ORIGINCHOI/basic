8203bdf2c519c2e05d349bf5075491389ec1ceec
Origin Choi <kyun4525@gmail.com>
2022-12-07 11:24:50 +0900

    bootstrap css 로딩, App.js 수정

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  let myName="김구라";
+  return (
+    <div className='container'>
+      <h1>인덱스 페이지 입니다.</h1>
+      {/* myName 이라는 변수 안에 있는 내용 사용하기 */}
+      <p>내이름은 <strong>{myName}</strong></p>
+      <button className='btn btn-primary'>눌러 보셈</button>
+    </div>
+  );
+}
+
+export default App;
